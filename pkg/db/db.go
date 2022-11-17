000ce0053c7eab31bbec6161ac1a770b154ab49e
saent-x <torsxperience@outlook.com>
2022-11-17 06:28:01 +0000

    removed pem and update db config

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Init() {
-	err := mgm.SetDefaultConfig(nil, "spreewill_lab", options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@localhost:27018", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))))
+	err := mgm.SetDefaultConfig(nil, "spreewill_lab", options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@mongo:27017", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))))
 
 	if err != nil {
 		log.Fatalln(err)
